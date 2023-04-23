42b56666b759c87993f58dae1dceb01cef7a4078
anassKEMMOUNE <53840240+ari-xoz@users.noreply.github.com>
2023-04-23 19:29:22 +0100

    initial commit : added MVC structure

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section class="main">
+    <div class="sidebar">
+            
+    </div>
+    <div class="main_block">
+
+    </div>
+    </section>
+    <form method="post" enctype="multipart/form-data" action ="Controller/upload.php">
+        <label for="fileToUpload">Please input your excel sheet</label>
+         <input type="file" name="fileToUpload" id="fileToUpload">
+         <input type="submit">
+
+    </form>
+
+</body>
+</html>
