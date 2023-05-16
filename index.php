9c570caee283d40912f6f7b17418e7684e03ed6d
anassKEMMOUNE <53840240+ari-xoz@users.noreply.github.com>
2023-05-16 09:14:28 +0100

    changed index file

--- index.php
+++ index.php
@@ -1,28 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <section class="main">
-    <div class="sidebar">
-            
-    </div>
-    <div class="main_block">
+<?php
+include 'View/head.html';
+?>
+<body class = 'main'>
 
-    </div>
-    </section>
-    <form method="post" enctype="multipart/form-data" action ="Controller/upload.php">
-        <label for="fileToUpload">Please input your excel sheet</label>
-         <input type="file" name="fileToUpload" id="fileToUpload">
-         <label for="fileToUpload2">Please input your second excel sheet</label>
-         <input type="file" name="fileToUpload2" id="fileToUpload2">
-         <input type="submit">
+    <?php
+        include "View/sidebar.html";
+
+    ?>
+    <div class="main_block">
+        <div class="upload_form">
+    <form method="post" enctype="multipart/form-data" action ="Controller/upload.php" class='file_input'>
+        <label >Please input your two excel sheets</label>
+        <div class="flex">
+        <input type="file" name="fileToUpload" id="fileToUpload" >
+        <input type="file" name="fileToUpload2" id="fileToUpload2" >
+
+        </div>
+        <input type="submit">
 
     </form>
+    </div>
+    </div>
+
+
 
 </body>
 </html>
